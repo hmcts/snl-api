438d59d2868ef70d68c590de1e97f780f8e5a8d9
Vijaya raghava Krishna shetty <vijayrkshetty@gmail.com>
2018-04-13 09:11:47 +0100

    added infrastructure and jenkins files

---
+++ infrastructure/main.tf
@@ -0,0 +1,17 @@
+locals {
+  app_full_name = "${var.product}-${var.component}"
+}
+module "snl-api" {
+  source               = "git@github.com:hmcts/moj-module-webapp"
+  product              = "${var.product}-${var.component}"
+  location             = "${var.location}"
+  env                  = "${var.env}"
+  ilbIp                = "${var.ilbIp}"
+  is_frontend          = false
+  subscription         = "${var.subscription}"
+  additional_host_name = "${var.external_host_name}"
+
+  app_settings = {
+
+  }
+}
