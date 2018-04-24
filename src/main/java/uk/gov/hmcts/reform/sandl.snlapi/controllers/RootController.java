389a069bf685ed0fbb6eca17d759a763380e957c
radoslawlandowskikainos <r.landowski@kainos.com>
2018-04-24 09:37:28 +0200

    Move endpoint to separate controller

--- src/main/java/uk/gov/hmcts/reform/sandl.snlapi/controllers/RootController.java
+++ src/main/java/uk/gov/hmcts/reform/sandl.snlapi/controllers/RootController.java
@@ -1,12 +1,7 @@
 package uk.gov.hmcts.reform.sandl.snlapi.controllers;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.http.ResponseEntity.ok;
@@ -29,20 +24,5 @@ public class RootController {
     @GetMapping
     public ResponseEntity<String> welcome() {
         return ok("Welcome to snl-api");
-    }
-
-    @CrossOrigin
-    @RequestMapping(path = "/get-sessions", method = RequestMethod.GET, produces = "application/json")
-    @ResponseBody
-    public String getSessions(@RequestParam("date") String date) {
-        if (date.equals("15-04-2018")) {
-            return "[{\"date\": \"Wed Apr 11 2018 14:12:36 GMT+0100 (BST)\",\"room\": {\"name\": \"Room one\"},"
-                + "\"judge\": {\"name\":\"John Judge\"}, \"durationInMinutes\": 90, \"jurisdiction\":\"ABC\"},"
-                + "{\"date\": \"Wed Apr 11 2018 14:12:36 GMT+0100 (BST)\",\"room\": {\"name\": \"Room one\"},"
-                + "\"judge\":{\"name\":\"John Judge\"}, \"durationInMinutes\": 90, \"jurisdiction\":\"ABC\"}]";
-        } else {
-            return "[{\"date\": \"Wed Apr 11 2018 14:12:36 GMT+0100 (BST)\",\"room\": {\"name\": \"Room one\"},"
-                + "\"judge\":{\"name\":\"John Judge\"}, \"durationInMinutes\": 90, \"jurisdiction\":\"ABC\"}]";
-        }
     }
 }
