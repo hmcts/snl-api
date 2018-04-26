5001ce39cca089d086260618e935425516b5c649
Leszek Gurniewicz <l.gurniewicz@kainos.com>
2018-04-26 13:04:19 +0200

    SL-772 Chekstyle fixes

--- src/test/java/uk/gov/hmcts/reform/sandl/snlapi/controllers/GetWelcomeTest.java
+++ src/test/java/uk/gov/hmcts/reform/sandl/snlapi/controllers/GetWelcomeTest.java
@@ -4,15 +4,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-
-import javax.servlet.Filter;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
