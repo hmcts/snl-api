9e254d7de237462ade1b772cb8044c429b4aefdb
Leszek Gurniewicz <l.gurniewicz@kainos.com>
2018-04-27 12:14:51 +0200

    SL-772 related bugfix. Added CORS related changes

--- src/main/java/uk/gov/hmcts/reform/sandl/snlapi/Application.java
+++ src/main/java/uk/gov/hmcts/reform/sandl/snlapi/Application.java
@@ -1,10 +1,15 @@
 package uk.gov.hmcts.reform.sandl.snlapi;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
@@ -17,5 +22,25 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public CorsFilter corsFilter(
+        @Value("${management.security.allowedOrigin}") String allowedOrigins,
+        @Value("${management.security.allowedHeaders}") String allowedHeaders
+    ) {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true); // you USUALLY want this
+        // likely you should limit this to specific origins
+        config.addAllowedOrigin(allowedOrigins);
+        config.addAllowedHeader(allowedHeaders);
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/logout", config);
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
