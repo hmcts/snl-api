6223589eaae2e6e7b254582de4b02f66265e4dd7
Remigiusz Kruk <R.Kruk@kainos.com>
2018-05-07 12:17:37 +0200

    add endpoint for rooms and persons

---
+++ src/main/java/uk/gov/hmcts/reform/sandl/snlapi/controllers/PersonController.java
@@ -0,0 +1,31 @@
+package uk.gov.hmcts.reform.sandl.snlapi.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import uk.gov.hmcts.reform.sandl.snlapi.services.EventsCommunicationService;
+
+@RestController
+@RequestMapping("/persons")
+public class PersonController {
+
+    @Autowired
+    private EventsCommunicationService eventsCommunicationService;
+
+    @GetMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public String getPersons() {
+        return eventsCommunicationService.makeCall("/persons", HttpMethod.GET).getBody();
+    }
+
+    @GetMapping(path = "", params = "personType", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public String getJudges(@RequestParam("personType") String personType) {
+        return eventsCommunicationService.makeCall("/persons?personType={personType}", HttpMethod.GET, personType).getBody();
+    }
+}
