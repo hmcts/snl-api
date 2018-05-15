fc1875139bd7e5ee095fd79aff14011ebc8c30f3
Remigiusz Kruk <R.Kruk@kainos.com>
2018-05-15 11:29:09 +0200

    add problems controller

---
+++ src/main/java/uk/gov/hmcts/reform/sandl/snlapi/controllers/ProblemController.java
@@ -0,0 +1,25 @@
+package uk.gov.hmcts.reform.sandl.snlapi.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import uk.gov.hmcts.reform.sandl.snlapi.services.EventsCommunicationService;
+
+@RestController
+@RequestMapping("/problems")
+public class ProblemController {
+
+    @Autowired
+    EventsCommunicationService eventsCommunicationService;
+
+    @GetMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public String getProblems() {
+        return eventsCommunicationService.makeCall("/problems", HttpMethod.GET).getBody();
+    }
+
+}
