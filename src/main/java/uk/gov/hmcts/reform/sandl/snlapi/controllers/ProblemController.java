f613d7d42abc378e6243e2b414c2b14f600be612
Remigiusz Kruk <R.Kruk@kainos.com>
2018-05-25 09:24:32 +0200

    add endpoint for getting problems by reference id

--- src/main/java/uk/gov/hmcts/reform/sandl/snlapi/controllers/ProblemController.java
+++ src/main/java/uk/gov/hmcts/reform/sandl/snlapi/controllers/ProblemController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.reform.sandl.snlapi.services.EventsCommunicationService;
@@ -20,6 +21,14 @@ public class ProblemController {
     @ResponseBody
     public String getProblems() {
         return eventsCommunicationService.makeCall("/problems", HttpMethod.GET).getBody();
+    }
+
+
+    @GetMapping(path = "by-reference-type-id", params = "id", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    String getProblemsByReferenceTypeId(@RequestParam("id") String id) {
+        return eventsCommunicationService.makeCall("/problems/by-reference-type-id?id={id}",
+            HttpMethod.GET, id).getBody();
     }
 
 }
