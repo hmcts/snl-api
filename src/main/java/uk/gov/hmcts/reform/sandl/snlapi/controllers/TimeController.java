fc28ca25d4ee342f1b473f104fae8b3d5972219d
Remigiusz Kruk <R.Kruk@kainos.com>
2018-05-25 08:33:57 +0200

    add requestmapping to time controller

--- src/main/java/uk/gov/hmcts/reform/sandl/snlapi/controllers/TimeController.java
+++ src/main/java/uk/gov/hmcts/reform/sandl/snlapi/controllers/TimeController.java
@@ -5,10 +5,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.reform.sandl.snlapi.services.EventsCommunicationService;
 
 @RestController
+@RequestMapping("/time")
 public class TimeController {
 
     @Autowired
