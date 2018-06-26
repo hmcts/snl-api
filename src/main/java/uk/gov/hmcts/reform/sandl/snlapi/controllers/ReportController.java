cba6a84d96f5050f83ffa4630e182ddfcbb0f91f
radoslawlandowskikainos <r.landowski@kainos.com>
2018-06-26 14:30:53 +0200

    endpoint for reports

--- src/main/java/uk/gov/hmcts/reform/sandl/snlapi/controllers/ReportController.java
+++ src/main/java/uk/gov/hmcts/reform/sandl/snlapi/controllers/ReportController.java
@@ -3,10 +3,7 @@ package uk.gov.hmcts.reform.sandl.snlapi.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import uk.gov.hmcts.reform.sandl.snlapi.services.EventsCommunicationService;
 
 @RestController
@@ -21,5 +18,12 @@ public class ReportController {
     public String getUnlistedHearingRequests() {
         return eventsCommunicationService.makeCall("/report/unlisted-hearing-requests",
             HttpMethod.GET).getBody();
+    }
+
+    @GetMapping(path = "listed-hearing-requests", params = {"startDate", "endDate"}, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public String getListedHearingRequests(@RequestParam("startDate") String startDate, @RequestParam("endDate") String endDate) {
+        return eventsCommunicationService.makeCall("/report/listed-hearing-requests?startDate={startDate}&endDate={endDate}",
+            HttpMethod.GET, startDate, endDate).getBody();
     }
 }
