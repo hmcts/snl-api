353a019069633e3be81a3885d90d5266a0226559
Remigiusz Kruk <R.Kruk@kainos.com>
2018-07-16 16:12:04 +0200

    add security tests

--- src/main/java/uk/gov/hmcts/reform/sandl/snlapi/security/requests/LoginRequest.java
+++ src/main/java/uk/gov/hmcts/reform/sandl/snlapi/security/requests/LoginRequest.java
@@ -1,10 +1,14 @@
 package uk.gov.hmcts.reform.sandl.snlapi.security.requests;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginRequest {
     @NotNull
     private String username;
