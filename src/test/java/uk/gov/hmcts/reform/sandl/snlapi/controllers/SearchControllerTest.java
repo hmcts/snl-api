9a995f689ca54934905bb67dd13881d604c7c210
radoslawlandowskikainos <r.landowski@kainos.com>
2018-07-17 11:55:10 +0200

    unused imps

--- src/test/java/uk/gov/hmcts/reform/sandl/snlapi/controllers/SearchControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/sandl/snlapi/controllers/SearchControllerTest.java
@@ -16,11 +16,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import uk.gov.hmcts.reform.sandl.snlapi.services.EventsCommunicationService;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
