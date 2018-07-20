6a9127f5c3f52440a29f94e7661f4cbccc91d931
radoslawlandowskikainos <r.landowski@kainos.com>
2018-07-20 14:51:42 +0200

    Set url for test when using fake events

--- src/test/java/integration/fakeevents/BaseIntegrationTestWithFakeEvents.java
+++ src/test/java/integration/fakeevents/BaseIntegrationTestWithFakeEvents.java
@@ -2,7 +2,11 @@ package integration.fakeevents;
 
 import integration.BaseIntegrationTest;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.test.context.TestPropertySource;
 
+@TestPropertySource(properties = {
+    "communication.eventsUrl=http://localhost:8092",
+})
 @AutoConfigureWireMock(port = 8092)
 public abstract class BaseIntegrationTestWithFakeEvents extends BaseIntegrationTest {
 }
