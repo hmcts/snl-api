dbcb17ebdefb2a5eeb51ac248173d26df02e6020
radoslawlandowskikainos <r.landowski@kainos.com>
2018-07-20 12:48:18 +0200

    Use proper wiremock rule

--- src/test/java/integration/BaseIntegrationTest.java
+++ src/test/java/integration/BaseIntegrationTest.java
@@ -1,31 +1,60 @@
 package integration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import com.github.tomakehurst.wiremock.junit.WireMockClassRule;
 import io.restassured.RestAssured;
+import io.restassured.http.Header;
+import io.restassured.response.Response;
 import org.junit.Before;
+import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.runner.RunWith;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import uk.gov.hmcts.reform.sandl.snlapi.Application;
+import uk.gov.hmcts.reform.sandl.snlapi.security.requests.LoginRequest;
+import uk.gov.hmcts.reform.sandl.snlapi.security.responses.JwtAuthenticationResponse;
+
+import static io.restassured.RestAssured.given;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT, classes = Application.class)
 public abstract class BaseIntegrationTest {
+
     public static final ObjectMapper objectMapper = new ObjectMapper();
+    public static final int EVENTS_PORT = 8193;
 
     @LocalServerPort
     int port;
 
+    @ClassRule
+    public static WireMockClassRule wireMockRule = new WireMockClassRule(EVENTS_PORT);
+
     @Rule
-    public WireMockRule wireMockRule = new WireMockRule(8193);
+    public WireMockClassRule instanceRule = wireMockRule;
 
     @Before
     public void before() {
         RestAssured.baseURI = "http://localhost:" + String.valueOf(port);
         RestAssured.useRelaxedHTTPSValidation();
+    }
+
+    public Header signIn(String username, String password) throws Exception {
+        Response signInResponse = given()
+            .contentType("application/json")
+            .body(objectMapper.writeValueAsString(new LoginRequest(username, password)))
+            .when()
+            .post("/security/signin")
+            .then()
+            .statusCode(200)
+            .and()
+            .extract().response();
+
+        JwtAuthenticationResponse token = objectMapper.readValue(signInResponse.body().print(),
+            JwtAuthenticationResponse.class);
+
+        return new Header("Authorization", "Bearer " + token.getAccessToken());
     }
 
 }
--- src/test/java/integration/controllers/RoomControllerIntTest.java
+++ src/test/java/integration/controllers/RoomControllerIntTest.java
@@ -2,21 +2,17 @@ package integration.controllers;
 
 import integration.BaseIntegrationTest;
 import io.restassured.http.Header;
-import io.restassured.response.Response;
 import org.junit.Test;
-import uk.gov.hmcts.reform.sandl.snlapi.security.requests.LoginRequest;
-import uk.gov.hmcts.reform.sandl.snlapi.security.responses.JwtAuthenticationResponse;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
 import static com.github.tomakehurst.wiremock.client.WireMock.getRequestedFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
-import static com.github.tomakehurst.wiremock.client.WireMock.urlMatching;
 import static com.github.tomakehurst.wiremock.client.WireMock.verify;
 import static io.restassured.RestAssured.given;
 
-public class RoomControllerTest extends BaseIntegrationTest {
+public class RoomControllerIntTest extends BaseIntegrationTest {
 
     @Test
     public void rooms_shouldCallProperEventsEndpointAndReturnRooms() throws Exception {
@@ -38,24 +34,6 @@ public class RoomControllerTest extends BaseIntegrationTest {
             .statusCode(200)
             .and().extract().response();
 
-        verify(getRequestedFor(urlMatching("/room")));
+        verify(getRequestedFor(urlEqualTo("/room")));
     }
-
-    private Header signIn(String username, String password) throws Exception {
-        Response signInResponse = given()
-            .contentType("application/json")
-            .body(objectMapper.writeValueAsString(new LoginRequest(username, password)))
-            .when()
-            .post("/security/signin")
-            .then()
-            .statusCode(200)
-            .and()
-            .extract().response();
-
-        JwtAuthenticationResponse token = objectMapper.readValue(signInResponse.body().print(),
-            JwtAuthenticationResponse.class);
-
-        return new Header("Authorization", "Bearer " + token.getAccessToken());
-    }
-
 }
