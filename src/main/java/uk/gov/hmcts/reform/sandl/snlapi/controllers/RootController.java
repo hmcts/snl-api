8eff1f5b386497126e0263603233eb94f5bcb239
Leszek Gurniewicz <l.gurniewicz@kainos.com>
2018-08-01 10:52:12 +0200

    SL-1001 CR update

--- src/main/java/uk/gov/hmcts/reform/sandl/snlapi/controllers/RootController.java
+++ src/main/java/uk/gov/hmcts/reform/sandl/snlapi/controllers/RootController.java
@@ -1,11 +1,9 @@
 package uk.gov.hmcts.reform.sandl.snlapi.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import uk.gov.hmcts.reform.sandl.snlapi.services.EventsCommunicationService;
 
 import static org.springframework.http.ResponseEntity.ok;
 
@@ -15,9 +13,6 @@ import static org.springframework.http.ResponseEntity.ok;
 @RestController()
 @RequestMapping()
 public class RootController {
-
-    @Autowired
-    private EventsCommunicationService eventsCommunicationService;
 
     /**
      * Root GET endpoint.
