ce21ccbf977d3f65c78bd18c7c020fcc6fb2f509
Kamil Pakur <k.pakur@kainos.com>
2018-10-11 11:45:54 +0200

    SL-1872 shared infra

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -11,7 +11,11 @@ locals {
 
   // Shared Resources
   vaultName = "${var.raw_product}-${local.envInUse}"
-  sharedResourceGroup = "${var.raw_product}-shared-${local.envInUse}"
+  sharedResourceGroup = "${var.raw_product}-shared-infrastructure-${local.envInUse}"
+  sharedAspName = "${var.raw_product}-${local.envInUse}"
+  sharedAspRg = "${var.raw_product}-shared-infrastructure-${local.envInUse}"
+  asp_name = "${(var.env == "preview" || var.env == "spreview") ? "null" : local.sharedAspName}"
+  asp_rg = "${(var.env == "preview" || var.env == "spreview") ? "null" : local.sharedAspRg}"
 }
 
 module "snl-api" {
@@ -24,6 +28,8 @@ module "snl-api" {
   additional_host_name = "${var.external_host_name != "" ? var.external_host_name : "null"}"
   subscription         = "${var.subscription}"
   appinsights_instrumentation_key = "${var.appinsights_instrumentation_key}"
+  asp_rg               = "${local.asp_rg}"
+  asp_name             = "${local.asp_name}"
   common_tags          = "${var.common_tags}"
 
   app_settings = {
