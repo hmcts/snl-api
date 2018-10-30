4d744b9e8d5e29e9a0220ee19370e99b94dcdcbf
Kamil Pakur <k.pakur@kainos.com>
2018-10-30 10:07:40 +0100

    fix test to use wiremock

---
+++ src/test/integration/uk/gov/hmcts/reform/sandl/snlapi/fakeevents/controllers/RoomControllerIntRealTest.java
@@ -0,0 +1,35 @@
+package uk.gov.hmcts.reform.sandl.snlapi.fakeevents.controllers;
+
+import io.restassured.http.Header;
+import org.junit.Test;
+import org.springframework.http.HttpStatus;
+import uk.gov.hmcts.reform.sandl.snlapi.fakeevents.BaseIntegrationTestWithFakeEvents;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.put;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static io.restassured.RestAssured.given;
+
+public class RoomControllerIntRealTest extends BaseIntegrationTestWithFakeEvents {
+
+    @Test
+    public void rooms_shouldCallProperEventsEndpointAndReturnRooms() throws Exception {
+        stubFor(put(urlEqualTo("/room"))
+            .willReturn(aResponse()
+                .withStatus(HttpStatus.OK.value())
+                .withHeader("Content-Type", "application/json")
+                .withBody("[]")));
+
+        Header authenticationHeader = loginHelper.signInAsOfficer();
+
+        given()
+            .contentType("application/json")
+            .header(authenticationHeader)
+        .when()
+            .get("/room")
+        .then()
+            .assertThat()
+            .statusCode(200);
+    }
+}
